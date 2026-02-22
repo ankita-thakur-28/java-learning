8526f4daf34593ed8da604ec68c396f1b5c8f0a6
ankita-thakur-28 <ankitathakurmfp@gmail.com>
2026-02-22 16:00:52 +0900

    java-learnings

---
+++ Area.java
@@ -0,0 +1,13 @@
+public class Area {
+    public static void main(String[] args ){
+    int length = 25;
+    int width = 50;
+    int area;
+    
+    area = length * width;
+
+    System.out.println("length is: " + length);
+    System.out.println("width is: " + width);
+    System.out.println("Area of the rectangle is: " + area);
+    }    
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main (String[] args){
+        String studentName = "Ankita Kumari";
+        int studentAge = 24;
+        float studentFee = 75.2f;
+        char studentGrade = 'A';
+
+        System.out.println("student name: " + studentName);
+        System.out.println("student Age: " + studentAge);
+        System.out.println("student Fee: " + studentFee);
+        System.out.println("student Grade: " + studentGrade);
+    }
+     
+}
