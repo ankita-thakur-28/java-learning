3c28c2e303d5d7d7276976c8ae7712b70889cd02
ankita-thakur-28 <ankitathakurmfp@gmail.com>
2026-02-24 19:27:42 +0900

    fundamentals

---
+++ Array.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class Array {
+    public static void main (String[] args){
+        int[] numbers = {5, 6, 7, 8, 9 };
+        System.out.println(numbers.length);
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
--- Main.java
+++ Main.java
@@ -1,14 +1,7 @@
 public class Main {
     public static void main (String[] args){
-        String studentName = "Ankita Kumari";
-        int studentAge = 24;
-        float studentFee = 75.2f;
-        char studentGrade = 'A';
-
-        System.out.println("student name: " + studentName);
-        System.out.println("student Age: " + studentAge);
-        System.out.println("student Fee: " + studentFee);
-        System.out.println("student Grade: " + studentGrade);
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
     }
-     
 }
